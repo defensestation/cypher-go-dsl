ae004bbfd869c0c1d628f07029f325693b5f3e9b
gsingh-ds <gsingh@defensestation.ca>
2024-01-18 14:52:24 +0000

    empty properties added on create node

--- node.go
+++ node.go
@@ -70,6 +70,7 @@ func NodeCreate2(accountId string) Node {
 		accountId: accountId,
 		labels: labels,
 		notNil: true,
+		Properties: Properties{},
 	}
 	node.injectKey()
 	return node
@@ -94,6 +95,7 @@ func NodeCreate3(accountId string, additionalLabel ...string) Node {
 	node := Node{
 		accountId: accountId,
 		labels: labels,
+		Properties: Properties{},
 	}
 	node.injectKey()
 	return node
