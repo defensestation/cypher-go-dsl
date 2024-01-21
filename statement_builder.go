0635c180443959b5db6ec150d7a882aa27fe51d9
gsingh-ds <gsingh@defensestation.ca>
2024-01-21 19:33:41 +0000

    SetMutateWithNamed option added

--- statement_builder.go
+++ statement_builder.go
@@ -150,6 +150,7 @@ type ExposesWith interface {
 type ExposesSet interface {
 	Set(expressions ...Expression) BuildableStatementAndOngoingMatchAndUpdate
 	SetWithNamed(variable Named, expression Expression) BuildableStatementAndOngoingMatchAndUpdate
+	SetMutateWithNamed(variable Named, expression Expression) BuildableStatementAndOngoingMatchAndUpdate
 }
 
 type ExposesSetAndRemove interface {
@@ -186,6 +187,7 @@ type OngoingMergeAction interface {
 	GetErr() error
 	Set(expressions ...Expression) OngoingMatchAndUpdateAndBuildableStatementAndExposesMergeAction
 	SetWithNamed(variable Named, expression Expression) OngoingMatchAndUpdateAndBuildableStatementAndExposesMergeAction
+	SetMutateWithNamed(variable Named, expression Expression) OngoingMatchAndUpdateAndBuildableStatementAndExposesMergeAction
 }
 
 type OngoingMatchAndReturnWithOrder interface {
